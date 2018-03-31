adba9f04e696dd687b6b8febd5ecf2f3d40a52df
Kevin Stotz <kevin_stotz@yahoo.com>
2018-03-31 16:26:18 -0400

    init

--- DimeCoins/settings/prod.py
+++ DimeCoins/settings/prod.py
@@ -1,3 +1,22 @@
+
+WEBSITE_IP_ADDRESS = "127.0.0.1"
+WEBSITE_HOSTNAME = 'www.yogishouse.com'
+WEBSITE_PORT = 10004
+
+DASHBOARD_IP_ADDRESS = "127.0.0.1"
+DASHBOARD_HOSTNAME = 'dashboard.yogishouse.com'
+DASHBOARD_PORT = 10004
+
+LOCAL_HOST = 'localhost'
+LOCAL_HOST_PORT = 10004
+
+ENGINE_IP_ADDRESS = "127.0.0.1"
+ENGINE_DOMAIN = 'yogishouse.com'
+ENGINE_HOSTNAME = 'api' + "." + ENGINE_DOMAIN
+ENGINE_PORT = 10006
+
+
+
 from .base import *
 from os.path import join
 from os import environ
