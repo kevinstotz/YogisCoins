dc06f5ad51fdbceb7ad7cd78d6608f879c227ca7
Kevin Stotz <kevin_stotz@yahoo.com>
2018-04-01 10:15:19 -0400

    fix path

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+export PYTHONPATH=/opt/www/dime/api/DimeAPI
 
-/usr/local/bin/python /efs/www/dime/coins/DimeCoins/manage.py runserver 172.31.2.86:10007
+/usr/local/bin/python /opt/www/dime/coin/YogisCoins/manage.py runserver 172.31.2.86:10007
